ee9851d04ba8e19e97e16f30156d22d0a75b3170
Rachel Chen <rachelchen.522@gmail.com>
2016-02-18 19:47:50 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeCtr button").click(likeClick);
+}
+
+function likeClick(e) {
+	e.preventDefault();
+	console.log("like button clicked");
+	ga("send", "event", 'like', 'click');
 }
